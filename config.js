aa9d8b23db0e14498dc164c67929c4f09214adc2
Nithin Kodithala <kodithyalanithin152@gmail.com>
2026-06-21 16:19:52 +0530

    added

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ const CONFIG = {
   RESTAURANT_NAME: "Sr Grand Hotel Grand Multi Cuisine Restaurant",
   RESTAURANT_PHONE: "+919030740810",
   RESTAURANT_WHATSAPP: "+919030740810",
-  RESTAURANT_MAP_URL: "https://maps.app.goo.gl/gjzW2NoeWodzHWrx7",
-  RESTAURANT_ADDRESS: "1st Floor, Sr Grand Hotel Complex",
+  RESTAURANT_MAP_URL: "https://maps.app.goo.gl/vNvWPRRiATstaaPC6",
+  RESTAURANT_ADDRESS: "1st Floor, RTC Bus Stand",
   OPEN_TIME: "11:00 AM",
   CLOSE_TIME: "11:00 PM",
   ADMIN_EMAIL: "admin@srgrandhotel.com",
-  ADMIN_PASSWORD: "chalukya@2024"
+  ADMIN_PASSWORD: "srgrand@2021"
 };
